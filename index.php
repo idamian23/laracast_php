0a6bcf4317b8df2e79e9110c77fa34744b6928d4
idamian <damian.ionut23@yahoo.ro>
2024-02-10 18:57:58 +0200

    partials

--- index.php
+++ index.php
@@ -1,42 +1,5 @@
 <?php
-$books = [
-	[
-		'name' => 'Do Androids Dream of Electric Sheep',
-		'author' => 'Philip K. Dick',
-		'releaseYear' => 1968,
-		'purchaseUrl' => 'http://example.com'
-	],
-	[
-		'name' => 'Project Hail Mary',
-		'author' => 'Andy Weir',
-		'releaseYear' => 2021,
-		'purchaseUrl' => 'http://example.com'
-	],
-	[
-		'name' => 'The Martians',
-		'author' => 'Andy Weir',
-		'releaseYear' => 2011,
-		'purchaseUrl' => 'http://example.com'
-	],
-];
 
+$heading = "Home";
 
-function filter($items, $fn)
-{
-	$filteredItems = [];
-
-	foreach ($items as $item) {
-		if ($fn($item)) {
-			$filteredItems[] = $item;
-		}
-	}
-
-	return $filteredItems;
-}
-
-$filteredBooks = array_filter($books, function ($book) {
-	return $book['author'] === 'Andy Weir';
-});
-
-
-require "index.view.php";
+require "views/index.view.php";
