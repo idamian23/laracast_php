cc48e2593c2ff6251f51b881ad59c5243961971f
idamian <damian.ionut23@yahoo.ro>
2024-02-17 19:15:54 +0200

    SQL Injection and Vulnerabilities

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+return ['database' => ['host' => 'localhost', 'port' => 3306, 'dbname' => 'myapp', 'charset' => 'utf8mb4',],
+    //services | api's
+    ];
--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
 require('functions.php');
-require('router.php');
+require('Database.php');
+//require('router.php');
+// @formatter:on
+//connect to our MySQL database.
+
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$id = $_GET['id'];
+$query = "select * from posts where id = :id";
+$posts = $db->query($query, [':id' => $id])->fetch();
+
+dd($posts);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post["title"] . "</li>";
+}
