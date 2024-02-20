0e732af895a247d139decdbab7f8f3db209e5f12
idamian <damian.ionut23@yahoo.ro>
2024-02-20 23:07:48 +0200

    render notes and note pages

--- index.php
+++ index.php
@@ -2,19 +2,7 @@
 
 require('functions.php');
 require('Database.php');
-//require('router.php');
+require('router.php');
 // @formatter:on
 //connect to our MySQL database.
 
-$config = require('config.php');
-$db = new Database($config['database']);
-
-$id = $_GET['id'];
-$query = "select * from posts where id = :id";
-$posts = $db->query($query, [':id' => $id])->fetch();
-
-dd($posts);
-
-foreach ($posts as $post) {
-    echo "<li>" . $post["title"] . "</li>";
-}
