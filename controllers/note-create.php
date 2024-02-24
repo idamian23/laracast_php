de576bd766130191fc7d26b861041625ece79994
idamian <damian.ionut23@yahoo.ro>
2024-02-24 20:40:33 +0200

    form validatation

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,15 +1,26 @@
 <?php
 $config = require ('config.php');
 $db = new Database($config['database']);
+require 'Validator.php';
 
 $heading = "Create a note";
 
 if($_SERVER["REQUEST_METHOD"] === 'POST') {
-$db->query('insert into notes(body, user_id) values(:body, :user_id)',
-    [
-    'body'=>$_POST['body'],
-    'user_id'=> 1,
-    ]);
+
+    $errors = [];
+
+
+    if(! Validator::string($_POST['body'] , 3 , 100)) {
+        $errors['body'] = "A body of no more than 100 characters is required!";
+    }
+
+    if(empty($errors)){
+        $db->query('insert into notes(body, user_id) values(:body, :user_id)',
+            [
+                'body'=>$_POST['body'],
+                'user_id'=> 1,
+            ]);
+    }
 }
 
 require "views/note-create.view.php";
